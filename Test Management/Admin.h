6fa1f04bae17602b1539b920cc5907654d945652
attaullahbhatti <55228804+attaullahbhatti@users.noreply.github.com>
2019-10-17 19:16:07 +0500

    Enhanced code readability
    Added comments in admin.h  so that its should become easy to read.And i think it is.

--- Test Management/Admin.h
+++ Test Management/Admin.h
@@ -1,18 +1,23 @@
+// libararies need for file to work properly
 #include <iostream>
 using namespace std;
+// person header file to access its functions
 # include "Person.h"
-
+// Admin Class
 class Admin : public Person
 {
 private:
+ // login and password
 	string loginPassword;
 	const string correctLoginPassword = "abcd";
 	const string correctCNIC = "12345";
 protected:
+ // question Authority function
 	void appendQuestion();
 	void adminDeleteQuestion();
 	bool checkLogin();
 public:
+ // login setter and getters
 	void setLoginPassword(string);
 	string getLoginPassword();
 	void Login();
